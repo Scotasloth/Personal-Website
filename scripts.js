19ccd7476492cdfcdc6665c9c6d17dc874aac771
Scotasloth <rossmcc76@gmail.com>
2024-10-26 19:44:51 +0100

    Creating drop down menu

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const dropDownButton = document.querySelector('.dropbtn')
+    const dropDownContent = document.getElementById('dropdown-content')
+
+    dropDownButton.addEventListener('click', () => {
+        dropDownContent.classList.toggle('show');
+    });
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+           if (dropDownContent.classList.contains('show')) {
+            dropDownContent.classList.remove('show');
+           } 
+        }
+    }
+});

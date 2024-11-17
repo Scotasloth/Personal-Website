47c676ebc8643b5d1fe82488bf6736fb0da1bc33
Scotasloth <rossmcc76@gmail.com>
2024-11-17 17:08:56 +0000

    Changed text of link to non-code projects page

--- Web Updater/Apps/models.py
+++ Web Updater/Apps/models.py
@@ -1,10 +1,5 @@
 
 from django.db import models
 
-class Update(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
+class post (models.Model):
+    
